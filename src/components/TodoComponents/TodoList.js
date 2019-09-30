9b2156c48d514853f230ca337043c739031272d5
Leelsmuth <femianjorin@rocketmail.com>
2019-09-30 13:46:33 +0100

    MVP completed

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = props => {
+  return (
+    <div className="todo-tasks">
+      {props.toDoItems.map(item => (
+        <Todo key={item.id} taskData={item} toggleTask={props.toggleTask} />
+      ))}
+      <button onClick={props.clearFinished}>Clear Purchased</button>
+    </div>
+  );
+};
+
+export default TodoList;
